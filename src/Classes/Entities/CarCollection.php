366e46f19e560d8bfb54bf0821d2226e3337f112
Andrew Scott <andrewgscott@gmail.com>
2022-05-16 14:27:02 +0100

    add CarHydrator

--- src/Classes/Entities/CarCollection.php
+++ src/Classes/Entities/CarCollection.php
@@ -4,5 +4,15 @@ namespace CarpeDiem\Classes\Entities;
 
 class CarCollection
 {
+    private array $cars = [];
 
+    public function getCollectedCars(): array
+    {
+        return $this->cars;
+    }
+
+    public function setCollectedCars(array $collectedCars): void
+    {
+        $this->cars = $collectedCars;
+    }
 }
--- src/Classes/Hydrators/CarHydrator.php
+++ src/Classes/Hydrators/CarHydrator.php
@@ -5,4 +5,18 @@ namespace CarpeDiem\Classes\Hydrators;
 class CarHydrator
 {
 
+    public static function hydrateFromArray($carArray)
+    {
+        $car = new Car();
+
+        $car->setId($carArray['id']);
+        $car->setId($carArray['make']);
+        $car->setId($carArray['model']);
+        $car->setId($carArray['year']);
+        $car->setId($carArray['colour']);
+        $car->setId($carArray['location']);
+        $car->setId($carArray['image']);
+
+        return $car;
+    }
 }
