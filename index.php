7d32c658764461f16f7b95ed00779886817a7560
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-05-17 11:29:44 +0100

    final styling

--- index.php
+++ index.php
@@ -1,1 +1,110 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="csss/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div class="jumbo">
+        <div class="jumbo-container">
+            <h1 class="title">CarPe-Diem</h1>
+            <p class="sub-heading">Car Collection</p>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="cars">
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image1.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image2.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image3.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image4.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image5.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image6.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image7.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+        <div class="imgGrid">
+            <img src="documents/images/background.jpg" class="carBlack">
+            <img src="documents/images/image8.png" class="carImg">
+            <div class="details">
+                <h3 class="make">Make</h3>
+                <h3 class="model">Model</h3>
+            </div>
+            <button class="moreBtn">See More</button>
+        </div>
+
+    </div>
+</main>
+</body>
+</html>
