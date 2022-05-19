f96904c0eeab8ca17b8e735cc8932f4b66f1836d
Andrew Scott <andrewgscott@gmail.com>
2022-05-19 12:33:13 +0100

    added message when no cars returned

--- src/Classes/ViewHelpers/CarViewHelper.php
+++ src/Classes/ViewHelpers/CarViewHelper.php
@@ -5,20 +5,27 @@ class CarViewHelper
 {
     public static function showCollection(array $showCollection): string
     {
-        $carStr ='';
-        foreach ($showCollection as $car) {
-            $carId = $car->getId();
-            $carStr .= '<div class="imgGrid">'
-                . '<img src="documents/images/background.jpg" class="carBlack" alt="black background">'
-                . '<img src="documents/images/' . $car->getImage() . '" class="carImg" alt=" Car image">'
-                . '<div class="details">'
-                . '<h3 class="make">' . $car->getMake() . '</h3>'
-                . '<h3 class="model">' . $car->getModel() . '</h3>'
-                . '</div>'
-                . '<form method="post" action="cardetail.php">'
-                . '<button name="carId" type="submit" class="moreBtn" value="'. $carId .'">See More</button>'
-                . '</form>'
-                . '</div>'; }
+        $carStr = '';
+        if (!$showCollection) {
+            $carStr = 'Sorry no cars to display';
+        } else {
+
+            foreach ($showCollection as $car) {
+                $carId = $car->getId();
+                $carStr .= '<div class="imgGrid">'
+                    . '<img src="documents/images/background.jpg" class="carBlack" alt="black background">'
+                    . '<img src="documents/images/' . $car->getImage() . '" class="carImg" alt=" Car image">'
+                    . '<div class="details">'
+                    . '<h3 class="make">' . $car->getMake() . '</h3>'
+                    . '<h3 class="model">' . $car->getModel() . '</h3>'
+                    . '</div>'
+                    . '<form method="post" action="cardetail.php">'
+                    . '<button name="carId" type="submit" class="moreBtn" value="' . $carId . '">See More</button>'
+                    . '</form>'
+                    . '</div>';
+            }
+
+        }
         return $carStr;
     }
 }
