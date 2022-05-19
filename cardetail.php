d5e69993b1982de560f82c9513eb3f2a353b7998
alech <hamiltonalec@gmail.com>
2022-05-19 12:10:07 +0100

    added some styling

--- cardetail.php
+++ cardetail.php
@@ -18,6 +18,8 @@ $car = $carService->getCar($carId);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/styleDetail.css">
@@ -26,11 +28,9 @@ $car = $carService->getCar($carId);
 
 <body>
 <header class="header-container">
-    <form action="index.php">
-        <button class="backBtn">
-            <<
-        </button>
-    </form>
+    <a href="index.php">
+        <span class="material-icons">west</span>
+    </a>
     <h1>CarPe-Diem | Car Collection</h1>
 </header>
 <main>
