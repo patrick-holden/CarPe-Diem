e4d8b0167e9945ea16a9ba65ef06e3ad5b47c244
Andrew Scott <andrewgscott@gmail.com>
2022-05-19 14:05:03 +0100

    fixed spacing

--- tests/Classes/ViewHelpers/SearchViewHelperTest.php
+++ tests/Classes/ViewHelpers/SearchViewHelperTest.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../../src/Classes/ViewHelpers/SearchViewHelper.php';
 
 use CarpeDiem\Classes\ViewHelpers\SearchViewHelper;
 use PHPUnit\Framework\TestCase;
+
 class SearchViewHelperTest extends TestCase
 {
     public function testdisplaySearchInput_givenString_returnsString() {
