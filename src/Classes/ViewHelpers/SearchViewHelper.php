6962e2cd3abf16d3f216cd689ea8db5493863521
alech <hamiltonalec@gmail.com>
2022-05-19 15:59:29 +0100

    fixed failed test

--- src/Classes/ViewHelpers/SearchViewHelper.php
+++ src/Classes/ViewHelpers/SearchViewHelper.php
@@ -7,7 +7,6 @@ class SearchViewHelper
     public static function setPostToSearchInput(string $lastSearch): string
     {
         $searchFunc = '<form action="index.php" method="post">';
-        $searchFunc .= '<label for="search"></label>';
         $searchFunc .= '<input type="text" name="search" placeholder="Search the collection here" id="search" value="' . htmlentities($lastSearch) .'">';
         $searchFunc .= '<button>Search</button>';
         $searchFunc .='</form>';
